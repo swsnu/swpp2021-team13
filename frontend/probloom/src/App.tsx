233309b4640761316fd25645090852187e1ba068
TLB-MISS <bsw1907@.naver.com>
2021-10-25 20:51:15 +0900

    front-end done

--- frontend/probloom/src/App.tsx
+++ frontend/probloom/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import Welcome from './containers/Welcome/Welcome';
+import SignUp from './containers/SignUp/SignUp';
 
 function App(props) {
   return (
@@ -13,6 +14,11 @@ function App(props) {
             path="/login"
             exact
             render={() => <Welcome history={props.history} logo="ProbLoom" />}
+          />
+          <Route
+            path="/signup"
+            exact
+            render={() => <SignUp history={props.history} />}
           />
           <Redirect exact from="/" to="login" />
           <Route render={() => <h1>Not Found</h1>} />
