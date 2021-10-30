cba73252633fb533a0f21b4b8cb4c06941fe595a
JeonHyeong Lee <john713@hanmail.net>
2021-10-30 15:39:49 +0900

    Revert "Feat: Fixed profile statistics and tests"

--- frontend/probloom/src/components/Layout/Footer.tsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <div className="container-footer">I am Footer!</div>;
-};
-
-export default Footer;
--- frontend/probloom/src/containers/Profile/ProfileStatistics.tsx
+++ frontend/probloom/src/containers/Profile/ProfileStatistics.tsx
@@ -15,44 +15,28 @@ class ProfileStatistics extends Component<ProfileStatisticsProps> {
   }
 
   render() {
-    const lastActiveDays: number =
+    const lastActiveDays =
       this.props.selectedUserStatistics?.lastActiveDays ?? 0;
-    const createdProblems: number[] = this.props.selectedUserStatistics
-      ?.createdProblems ?? [0];
-    const solvedProblems: number[] = this.props.selectedUserStatistics
-      ?.solvedProblems ?? [0];
-    const recommendedProblems: number[] = this.props.selectedUserStatistics
-      ?.recommendedProblems ?? [0];
-    const createdExplanations: number[] = this.props.selectedUserStatistics
-      ?.createdExplanations ?? [0];
-    const recommendedExplanations: number[] = this.props.selectedUserStatistics
-      ?.recommendedExplanations ?? [0];
-
+    const problemsCreated =
+      this.props.selectedUserStatistics?.problemsCreated ?? 0;
+    const problemsSolved =
+      this.props.selectedUserStatistics?.problemsSolved ?? 0;
     return (
       <div className="ProfileStatistics">
-        <div className="last-active">
+        <div className="title">
+          <h1>ProfileStatistics Page</h1>
+        </div>
+        <div>
           <h1>Last Active</h1>
           {lastActiveDays} days ago
         </div>
-        <div className="created-problems">
-          <h1>Created Problems</h1>
-          {createdProblems}
+        <div>
+          <h1>Problems Created</h1>
+          {problemsCreated}
         </div>
-        <div className="solved-problems">
-          <h1>Solved Problems</h1>
-          {solvedProblems}
-        </div>
-        <div className="recommended-problems">
-          <h1>Recommended Problems</h1>
-          {recommendedProblems}
-        </div>
-        <div className="created-explanations">
-          <h1>Created Explanations</h1>
-          {createdExplanations}
-        </div>
-        <div className="recommended-explanations">
-          <h1>Recommended Explanations</h1>
-          {recommendedExplanations}
+        <div>
+          <h1>Problems Solved</h1>
+          {problemsSolved}
         </div>
       </div>
     );
