88552520a956d449c20ace407c01fa7d4e3f2e2a
Myeongseop Jeon <jeonms9338@snu.ac.kr>
2021-10-30 12:59:35 +0900

    Fix format

--- backend/probloom/prob/urls.py
+++ backend/probloom/prob/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.userList),
-    path('<int:id>/', views.userInfo),
+    path("", views.userList),
+    path("<int:id>/", views.userInfo),
 ]
--- backend/probloom/prob/views.py
+++ backend/probloom/prob/views.py
@@ -1,6 +1,11 @@
-#from django.shortcuts import render
-from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse, response 
-import json 
+# from django.shortcuts import render
+from django.http import (
+    HttpResponseBadRequest,
+    HttpResponseNotAllowed,
+    JsonResponse,
+    response,
+)
+import json
 from json.decoder import JSONDecodeError
 from django.views.decorators.csrf import ensure_csrf_cookie
 from .models import User
@@ -8,37 +13,40 @@ from .models import User
 # Create your views here.
 @ensure_csrf_cookie
 def userList(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         userAllList = [user for user in User.objects.all().values()]
         return JsonResponse(userAllList, safe=False)
-    elif request.method == 'POST':
+    elif request.method == "POST":
         try:
             body = request.body.decode()
-            username = json.loads(body)['username']
-            email = json.loads(body)['email']
-            password = json.loads(body)['password']
-            logged_in = json.loads(body)['logged_in']
-        except(KeyError, JSONDecodeError) as e:
+            username = json.loads(body)["username"]
+            email = json.loads(body)["email"]
+            password = json.loads(body)["password"]
+            logged_in = json.loads(body)["logged_in"]
+        except (KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
-        user = User(username=username, email=email, password=password, logged_in=logged_in)
+        user = User(
+            username=username, email=email, password=password, logged_in=logged_in
+        )
         user.save()
-        responseDict = {'id': user.id, 'username': user.username}
+        responseDict = {"id": user.id, "username": user.username}
         return JsonResponse(responseDict, status=201)
     else:
-        return HttpResponseNotAllowed(['GET', 'POST'])
+        return HttpResponseNotAllowed(["GET", "POST"])
+
 
 @ensure_csrf_cookie
 def userInfo(request, id=0):
-    if request.method == 'GET':
+    if request.method == "GET":
         filteredUserInfo = [user for user in User.objects.filter(id=id).values()]
         return JsonResponse(filteredUserInfo, safe=False)
-    elif request.method == 'PUT':
+    elif request.method == "PUT":
         try:
             body = request.body.decode()
-            username = json.loads(body)['username']
-            email = json.loads(body)['email']
-            password = json.loads(body)['password']
-            logged_in = json.loads(body)['logged_in']
+            username = json.loads(body)["username"]
+            email = json.loads(body)["email"]
+            password = json.loads(body)["password"]
+            logged_in = json.loads(body)["logged_in"]
         except (KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
         targetUser = User.objects.get(id=id)
@@ -47,8 +55,13 @@ def userInfo(request, id=0):
         targetUser.password = password
         targetUser.logged_in = logged_in
         targetUser.save()
-        responseDict = {'id': id, 'username': username, 'email': email, 'password': password, 'logged_in': logged_in}
+        responseDict = {
+            "id": id,
+            "username": username,
+            "email": email,
+            "password": password,
+            "logged_in": logged_in,
+        }
         return JsonResponse(responseDict, status=201)
     else:
-        return HttpResponseNotAllowed(['GET', 'PUT'])
-    
+        return HttpResponseNotAllowed(["GET", "PUT"])
