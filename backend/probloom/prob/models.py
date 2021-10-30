3bafbfb70c1fdd93be46e005388a07a8c0b24733
Myeongseop Jeon <jeonms9338@snu.ac.kr>
2021-10-30 13:02:05 +0900

    Change user model

--- backend/probloom/prob/models.py
+++ backend/probloom/prob/models.py
@@ -1,11 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=120, default='')
-    email = models.CharField(max_length=120, default='')
-    password = models.CharField(max_length=120, default='')
-    logged_in = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.username
+class User(AbstractUser):
+    login = models.BooleanField(default=False)
