168f8a85c16ee12c3a3cc0f2e410f590c2b5bad7
Zagoshipda <john713@hanmail.net>
2021-10-30 16:00:26 +0900

    Update: dropdown box fixed

--- frontend/probloom/src/components/Layout/ProfileDropdown.tsx
+++ frontend/probloom/src/components/Layout/ProfileDropdown.tsx
@@ -13,6 +13,7 @@ class ProfileDropdown extends React.Component<
   GoToProfileProps,
   GoToProfileState
 > {
+  container = React.createRef<HTMLInputElement>();
   constructor(props: any) {
     super(props);
 
@@ -21,15 +22,36 @@ class ProfileDropdown extends React.Component<
     };
   }
 
+  componentDidMount() {
+    document.addEventListener('mousedown', this.closeDropdownMenu);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.closeDropdownMenu);
+  }
+
   showDropdownMenu = () => {
     this.setState({ displayMenu: !this.state.displayMenu });
   };
 
+  closeDropdownMenu = (event: any) => {
+    if (
+      this.container.current &&
+      !this.container.current.contains(event.target)
+    ) {
+      this.setState({ displayMenu: false });
+    }
+  };
+
   render() {
+    // console.log('render', this.state.displayMenu);
     return (
-      <div className="ProfileDropdown">
-        <button id="profile-button" onClick={() => this.showDropdownMenu()}>
-          <span>username: {this.props.username} </span>
+      <div className="ProfileDropdown" ref={this.container}>
+        <button
+          id="profile-button"
+          onClick={() => this.showDropdownMenu()}
+          onBlur={(e) => this.closeDropdownMenu(e)}
+        >
+          <span>☰ username: {this.props.username} </span>
           <img
             src="https://item.kakaocdn.net/do/1b9aa4b7165a7d3822d2c2c81d0c37259f5287469802eca457586a25a096fd31"
             alt="User avatar"
@@ -37,7 +59,9 @@ class ProfileDropdown extends React.Component<
           />
         </button>
 
-        {this.state.displayMenu ? (
+        <div className="blank"> </div>
+
+        {this.state.displayMenu && (
           <ul>
             <li>
               <NavLink to="/user/1/summary">View profile</NavLink>
@@ -46,7 +70,7 @@ class ProfileDropdown extends React.Component<
               <NavLink to="/user/1/summary">Sign out</NavLink>
             </li>
           </ul>
-        ) : null}
+        )}
       </div>
     );
   }
