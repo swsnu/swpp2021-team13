93b6e909b0a8e81b533ba153559a102ac5e3e39b
TLB-MISS <bsw1907@.naver.com>
2021-11-08 14:45:40 +0900

    Fix sonar-cloud error for safe/unsafe HTTP

--- backend/probloom/prob/views.py
+++ backend/probloom/prob/views.py
@@ -17,82 +17,95 @@ from django.views.generic.detail import SingleObjectMixin
 from .models import User, UserProfile, UserStatistics
 
 # Create your views here.
+# decorator for checking allowed HTTP
+def require_GET(view_func):
+    def wrap(request, *args, **kwargs):
+        if request.method != "GET":
+            return HttpResponseNotAllowed(["GET"])
+        return view_func(request, *args, **kwargs)
+
+    return wrap
+
+
+def require_POST(view_func, required_fields=None):
+    def wrap(request, *args, **kwargs):
+        if request.method != "POST":
+            return HttpResponseNotAllowed(["POST"])
+        return view_func(request, *args, **kwargs)
+
+    return wrap
+
+
+@require_POST
 def signup(request):
-    if request.method == "POST":
-        try:
-            req_data = json.loads(request.body.decode())
-            username = req_data["username"]
-            email = req_data["email"]
-            password = req_data["password"]
-        except (KeyError, JSONDecodeError) as e:
-            return HttpResponseBadRequest()
+    try:
+        req_data = json.loads(request.body.decode())
+        username = req_data["username"]
+        email = req_data["email"]
+        password = req_data["password"]
+    except (KeyError, JSONDecodeError) as e:
+        return HttpResponseBadRequest()
 
-        user_set = User.objects.all()
-        for user in user_set:
-            if (user.username == username) or (user.email == email):
-                return HttpResponse(status=401)
+    user_set = User.objects.all()
+    for user in user_set:
+        if (user.username == username) or (user.email == email):
+            return HttpResponse(status=401)
 
-        User.objects.create_user(username=username, email=email, password=password)
-        new_user = User.objects.get(username=username)
+    User.objects.create_user(username=username, email=email, password=password)
+    new_user = User.objects.get(username=username)
+    res = {
+        "id": new_user.id,
+        "username": new_user.username,
+        "email": new_user.email,
+        "logged_in": False,
+    }
+    return JsonResponse(res, status=201, safe=False)
+
+
+@require_POST
+def signin(request):
+    try:
+        req_data = json.loads(request.body.decode())
+        id = req_data["id"]
+        password = req_data["password"]
+    except (KeyError, JSONDecodeError) as e:
+        return HttpResponseBadRequest()
+
+    user_set = User.objects.all()
+    isEmail = id.find("@") >= 0
+    try:
+        if isEmail:
+            username = User.objects.get(email=id).username
+        else:
+            username = id
+    except:
+        return HttpResponse(status=401)
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+
+        user_ = User.objects.get(username=username)
         res = {
-            "id": new_user.id,
-            "username": new_user.username,
-            "email": new_user.email,
-            "logged_in": False,
+            "id": user_.id,
+            "username": user_.username,
+            "email": user_.email,
+            "logged_in": True,
         }
+        print(res)
         return JsonResponse(res, status=201, safe=False)
     else:
-        return HttpResponseNotAllowed(["POST"])
+        return HttpResponse(status=401)
 
 
-def signin(request):
-    if request.method == "POST":
-        try:
-            req_data = json.loads(request.body.decode())
-            id = req_data["id"]
-            password = req_data["password"]
-        except (KeyError, JSONDecodeError) as e:
-            return HttpResponseBadRequest()
-
-        user_set = User.objects.all()
-        isEmail = id.find("@") >= 0
-        try:
-            if isEmail:
-                username = User.objects.get(email=id).username
-            else:
-                username = id
-        except:
-            return HttpResponse(status=401)
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-
-            user_ = User.objects.get(username=username)
-            res = {
-                "id": user_.id,
-                "username": user_.username,
-                "email": user_.email,
-                "logged_in": True,
-            }
-            print(res)
-            return JsonResponse(res, status=201, safe=False)
-        else:
-            return HttpResponse(status=401)
-    else:
-        return HttpResponseNotAllowed(["POST"])
-
-
+@require_GET
 def signout(request):
-    if request.method == "GET":
-        if request.user.is_authenticated:
-            logout(request)
-            return HttpResponse(status=204)
-        else:
-            return HttpResponse(status=401)
+    if request.user.is_authenticated:
+        logout(request)
+        return HttpResponse(status=204)
     else:
-        return HttpResponseNotAllowed(["GET"])
+        return HttpResponse(status=401)
 
 
 class UserProfileView(LoginRequiredMixin, SingleObjectMixin, View):
@@ -125,18 +138,16 @@ class UserProfileView(LoginRequiredMixin, SingleObjectMixin, View):
         return HttpResponse()
 
 
+@require_GET
 def userStatistics(request, id=0):
-    if request.method == "GET":
-        userStatistics = UserStatistics.objects.get(id=id)
-        return JsonResponse(
-            {"id": userStatistics.id, "lastActiveDays": userStatistics.lastActiveDays},
-            safe=False,
-        )
+    userStatistics = UserStatistics.objects.get(id=id)
+    return JsonResponse(
+        {"id": userStatistics.id, "lastActiveDays": userStatistics.lastActiveDays},
+        safe=False,
+    )
 
 
 @ensure_csrf_cookie
+@require_GET
 def token(request):
-    if request.method == "GET":
-        return HttpResponse(status=204)
-    else:
-        return HttpResponseNotAllowed(["GET"])
+    return HttpResponse(status=204)
