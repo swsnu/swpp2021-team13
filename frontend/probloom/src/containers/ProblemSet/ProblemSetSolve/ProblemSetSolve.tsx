e638aa274d30f7b40655e7c07ab1c6f86042a0b3
TLB-MISS <bsw1907@.naver.com>
2021-12-11 04:32:53 +0900

    Add ProblemSetSolveResult test & Fix GET problem/undefined error

--- frontend/probloom/src/containers/ProblemSet/ProblemSetSolve/ProblemSetSolve.tsx
+++ frontend/probloom/src/containers/ProblemSet/ProblemSetSolve/ProblemSetSolve.tsx
@@ -132,6 +132,7 @@ class ProblemSetSolve extends Component<
 
     if (this.state.checkNum === problemIDList.length) {
       this.setState({ isFinalSubmit: true });
+      return;
     }
 
     this.onClickNextButton();
